05a6d5d0feb6645328062e261009216f5787aa63
bryan <bryanmills86@yahoo.com>
2017-12-06 21:24:33 -0800

    Added a ProgressSpinner view class

---
+++ Toolbox/src/main/java/com/whompum/bonitatoolbox/toolbox/widgets/ProgressSpinner.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2017 Bryan A. Mills
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.whompum.bonitatoolbox.toolbox.widgets;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.whompum.bonitatoolbox.toolbox.R;
+
+/**
+ * Created by bryan on 12/6/2017.
+ */
+
+public class ProgressSpinner extends View {
+
+
+    public static final String DEBUG = "ProgressSpiner";
+
+    @DrawableRes
+    private static final int BACKGROUND_ID = R.drawable.progress_spin_animation;
+
+    public ProgressSpinner(Context context) {
+        super(context);
+        initBackground();
+    }
+
+
+    public ProgressSpinner(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initBackground();
+    }
+
+
+    public ProgressSpinner(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initBackground();
+    }
+
+
+    private void initBackground(){
+
+        if(Build.VERSION.SDK_INT >= 21)
+            setBackground(getContext().getDrawable(BACKGROUND_ID));
+        else
+            setBackground(getContext().getResources().getDrawable(BACKGROUND_ID));
+
+    }
+
+
+    public void start(){
+        ((AnimationDrawable)getBackground()).start();
+    }
+
+    public void stop(){
+        ((AnimationDrawable)getBackground()).stop();
+    }
+
+
+    /**
+     * Since we add the drawable progmatically, the size of the view can't be determined
+     * Thus if we pass wrap_content, it will take up all the size it can (we're extending view thats why)
+     * To get around this, we simply fetch the wanted size of the Drawable,
+     * and pass that to the super implementation along with the clients LayoutParams mode
+     * passing along these values is better than setting them ourselves because we can
+     * let the view handle padding and what not
+     *
+     * @param widthMeasureSpec holds the LayoutParams mode we wanted
+     * @param heightMeasureSpec holds the LayoutParams mode we wanted
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        final int width = getBackground().getIntrinsicWidth();
+        final int height = getBackground().getIntrinsicHeight();
+
+        final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        super.onMeasure(MeasureSpec.makeMeasureSpec(width, widthMode), MeasureSpec.makeMeasureSpec(height, heightMode));
+
+    }
+
+
+}
